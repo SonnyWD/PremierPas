b1f762d96b0c019554cdc0a2d19a2462dd41ff17
Sonny <sonnyterlonwd@gmail.com>
2025-06-23 00:39:22 +0200

    Ajout Swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import 'reflect-metadata';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,16 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Premier Pas API')
+    .setDescription('Documentation interactive de l’API REST de l’application Premier Pas')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
 
   app.useGlobalPipes(
     new ValidationPipe({
