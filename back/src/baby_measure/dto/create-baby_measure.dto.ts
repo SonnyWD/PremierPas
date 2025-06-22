ba30b4e85500fb7912f2b024aed6e83f584bdac3
Sonny <sonnyterlonwd@gmail.com>
2025-06-22 20:45:51 +0200

    ajout de optional sur le dto baby measures

--- back/src/baby_measure/dto/create-baby_measure.dto.ts
+++ back/src/baby_measure/dto/create-baby_measure.dto.ts
@@ -1,15 +1,18 @@
-import { IsNumber } from "class-validator";
+import { IsNumber, IsOptional } from "class-validator";
 
 export class CreateBabyMeasureDto {
   @IsNumber()
   babyId: number;
 
   @IsNumber()
+  @IsOptional()
   sizeCm?: number;
 
   @IsNumber()
+  @IsOptional()
   weightKg?: number;
 
   @IsNumber()
+  @IsOptional()
   temperatureC?: number;
 }
